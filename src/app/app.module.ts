68b40028aef408f62441e2a44ae6cc783cf33fbd
Serge Honderdos <serge.honderdos@sytac.io>
2022-05-02 11:19:16 +0200

    feat: Add fade in animation for driver standings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { OrdinalPipe } from './services/pipes/ordinal/ordinal.pipe';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, OrdinalPipe],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
