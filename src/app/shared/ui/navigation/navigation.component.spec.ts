d1de85873286cbaa5a28f828790417ee744da378
SERGE HONDERDOS <5478388+shonderdos@users.noreply.github.com>
2023-02-01 20:56:13 +0100

    refactor(unit-test): Remove unused imports

--- src/app/shared/ui/navigation/navigation.component.spec.ts
+++ src/app/shared/ui/navigation/navigation.component.spec.ts
@@ -1,4 +1,4 @@
-import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { NavigationComponent } from './navigation.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { By } from '@angular/platform-browser';
