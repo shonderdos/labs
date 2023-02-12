fd13c444ada415afc6b2aaba7b98dbad21aa9550
SERGE HONDERDOS <5478388+shonderdos@users.noreply.github.com>
2023-02-12 18:54:17 +0100

    feat(navigation): Make click area larger and add hover effect

---
+++ src/app/shared/ui/navigation-item/navigation-item.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  standalone: true,
+  selector: 'app-navigation-item',
+  template: ` <a [routerLink]="link">{{ name }}</a> `,
+  styles: [
+    `
+      a,
+      a:hover,
+      a:active,
+      a:visited {
+        color: var(--navigation-text-color);
+        text-decoration: none;
+        padding: 0 20px;
+        height: 48px;
+        display: inline-block;
+        line-height: 48px;
+        border-radius: 6px;
+      }
+
+      a:hover {
+        background-color: var(--navigation-hover-background-color);
+      }
+    `,
+  ],
+  imports: [RouterModule],
+})
+export class NavigationItemComponent {
+  @Input() link: string | undefined;
+  @Input() name: string | undefined;
+}
