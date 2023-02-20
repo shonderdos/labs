f8f407a52934860ad67bb311189f237bfe5bd841
SERGE HONDERDOS <5478388+shonderdos@users.noreply.github.com>
2023-02-20 18:03:49 +0100

    feature(lazy-loading): Add lazy loading for all pages

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -1,12 +1,9 @@
 import { Routes } from '@angular/router';
-import { DriverStandingsComponent } from './driver-standings/driver-standings.component';
-import { ConstructorStandingsComponent } from './constructor-standings/constructor-standings.component';
-import { SettingsComponent } from './settings/settings.component';
 
 export const routes: Routes = [
-  { path: 'drivers', component: DriverStandingsComponent },
-  { path: 'constructors', component: ConstructorStandingsComponent },
-  { path: 'settings', component: SettingsComponent },
+  { path: 'drivers', loadComponent: () => import('./driver-standings/driver-standings.component') },
+  { path: 'constructors', loadComponent: () => import('./constructor-standings/constructor-standings.component') },
+  { path: 'settings', loadComponent: () => import('./settings/settings.component') },
   { path: '', redirectTo: '/drivers', pathMatch: 'full' },
   { path: '**', redirectTo: '/drivers' },
 ];
