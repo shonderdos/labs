dedae14b5b42c53d4464db86e7e9b2afdf7784a3
SERGE HONDERDOS <5478388+shonderdos@users.noreply.github.com>
2023-06-09 20:50:41 +0200

    feature(login): add responsivenes to the login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   selector: 'app-root',
   template: ` <router-outlet></router-outlet> `,
+  styles: [
+    `
+      :host {
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        top: 0;
+      }
+    `,
+  ],
 })
 export class AppComponent {}
