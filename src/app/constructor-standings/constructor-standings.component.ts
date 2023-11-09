0520071fce779a8238c6bf166017b934aeb0df98
SERGE HONDERDOS <5478388+shonderdos@users.noreply.github.com>
2023-11-09 20:25:03 +0100

    refactor(core): Implement new control flow in templates

--- src/app/constructor-standings/constructor-standings.component.ts
+++ src/app/constructor-standings/constructor-standings.component.ts
@@ -1,21 +1,13 @@
 import { Component } from '@angular/core';
-import { animate, query, stagger, style, transition, trigger } from '@angular/animations';
 import { OrdinalPipe } from '../shared/pipes/ordinal/ordinal.pipe';
-import { AsyncPipe, NgClass, NgFor, NgIf, NgOptimizedImage } from '@angular/common';
+import { AsyncPipe, NgClass, NgOptimizedImage } from '@angular/common';
 import { StandingsCardComponent } from '../shared/ui/standings-card/standings-card.component';
 import { FirebaseService } from '../shared/services/firebase/firebase.service';
 
 @Component({
   standalone: true,
-  imports: [OrdinalPipe, NgClass, NgIf, NgFor, AsyncPipe, StandingsCardComponent, NgOptimizedImage],
+  imports: [OrdinalPipe, NgClass, AsyncPipe, StandingsCardComponent, NgOptimizedImage],
   selector: 'app-constructor-interfaces',
-  animations: [
-    trigger('listAnimation', [
-      transition('* => *', [
-        query(':enter', [style({ opacity: 0 }), stagger(20, [animate('0.20s', style({ opacity: 1 }))])]),
-      ]),
-    ]),
-  ],
   templateUrl: 'constructor-standings.component.html',
   styleUrls: ['./constructor-standings.component.scss'],
 })
--- src/app/layouts/layout.component.ts
+++ src/app/layouts/layout.component.ts
@@ -2,7 +2,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import VerticalComponent from './vertical/vertical.component';
 import EmptyComponent from './empty/empty.component';
-import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
@@ -10,7 +9,7 @@ import { NgIf } from '@angular/common';
   styleUrls: ['./layout.component.scss'],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [EmptyComponent, VerticalComponent, NgIf],
+  imports: [EmptyComponent, VerticalComponent],
 })
 export default class LayoutComponent {
   public layout = 'vertical';
