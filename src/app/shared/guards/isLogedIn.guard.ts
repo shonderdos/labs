c1a688313b5553722a604a18841e3009f602d381
SERGE HONDERDOS <5478388+shonderdos@users.noreply.github.com>
2023-11-13 22:22:33 +0100

    feature(auth): redirect logged in users from /login to /dashboard

--- src/app/shared/guards/isLogedIn.guard.ts
+++ src/app/shared/guards/isLogedIn.guard.ts
@@ -1,8 +1,16 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth/auth.service';
 import { map } from 'rxjs';
 
 export const isLoggedInGuard: CanActivateFn = () => {
-  return inject(AuthService).state.pipe(map((user) => !!user));
+  return inject(AuthService).state.pipe(map(Boolean));
+};
+
+export const isLoggedInAndRedirectGuard: (path: string) => CanActivateFn = (path: string) => () => {
+  const router = inject(Router);
+  return inject(AuthService).state.pipe(
+    map(Boolean),
+    map((isLoggedIn) => (isLoggedIn ? router.createUrlTree([path]) : true))
+  );
 };
