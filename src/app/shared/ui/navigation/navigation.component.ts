2d30a4ce0e13126688517af40aa4df4bbf1975e4
SERGE HONDERDOS <5478388+shonderdos@users.noreply.github.com>
2023-12-15 20:00:50 +0100

    feat(navigation): split up component

--- src/app/shared/ui/navigation/navigation.component.ts
+++ src/app/shared/ui/navigation/navigation.component.ts
@@ -5,23 +5,8 @@ import { NAVIGATION_TOKEN } from '../../../app.component';
 @Component({
   standalone: true,
   selector: 'app-navigation',
-  template: `
-    @for (item of navigationItems; track item.link) {
-      <app-navigation-item [icon]="item.icon" [link]="item.link" [name]="item.name" />
-    }
-  `,
-  styles: [
-    `
-      :host {
-        height: 80px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        background-color: var(--navigation-background-color);
-        box-shadow: var(--navigation-box-shadow);
-      }
-    `,
-  ],
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss'],
   imports: [NavigationItemComponent],
 })
 export class NavigationComponent {
