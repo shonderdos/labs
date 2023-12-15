026e89ffa25abdf5ba57dc4523b39657aad578a7
SERGE HONDERDOS <5478388+shonderdos@users.noreply.github.com>
2023-12-15 19:55:52 +0100

    feat(navigation-item): split up component into separate files

--- src/app/shared/ui/navigation-item/navigation-item.component.ts
+++ src/app/shared/ui/navigation-item/navigation-item.component.ts
@@ -5,31 +5,8 @@ import { MatIconModule } from '@angular/material/icon';
 @Component({
   standalone: true,
   selector: 'app-navigation-item',
-  template: `
-    <div role="button" [routerLink]="link">
-      <mat-icon class="material-icons-outlined">{{ icon }}</mat-icon>
-      <span>{{ name }}</span>
-    </div>
-  `,
-  styles: [
-    `
-      div {
-        display: flex;
-        align-items: center;
-        border-radius: 6px;
-        padding: 0 20px;
-        color: var(--navigation-text-color);
-        height: 48px;
-      }
-      div:hover {
-        cursor: pointer;
-        background-color: var(--navigation-hover-background-color);
-      }
-      mat-icon {
-        margin-right: 10px;
-      }
-    `,
-  ],
+  templateUrl: './navigation-item.component.html',
+  styleUrls: ['./navigation-item.component.scss'],
   imports: [RouterLink, MatIconModule],
 })
 export class NavigationItemComponent {
