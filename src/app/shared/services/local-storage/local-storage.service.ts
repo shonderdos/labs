40f82dde57a21bf30200606f40c590b3dea53d52
SERGE HONDERDOS <5478388+shonderdos@users.noreply.github.com>
2023-12-15 20:31:21 +0100

    feature(localstorage): add unit tests to the service

--- src/app/shared/services/local-storage/local-storage.service.ts
+++ src/app/shared/services/local-storage/local-storage.service.ts
@@ -3,19 +3,10 @@ import { Injectable } from '@angular/core';
 @Injectable({ providedIn: 'root' })
 export class LocalStorageService {
   public get<T>(key: string): T | null {
-    const value = localStorage.getItem(key);
-
-    if (value === 'true' || value === 'false') {
-      return JSON.parse(value);
-    }
-
-    return value as T;
+    return localStorage.getItem(key) as T;
   }
 
-  public set(key: string, value: string | boolean): void {
-    if (typeof value === 'boolean') {
-      value = value.toString();
-    }
+  public set(key: string, value: string): void {
     localStorage.setItem(key, value);
   }
 }
