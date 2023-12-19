b5075d9f6539870573e8cd70d1dee9fa8bdb0b4e
SERGE HONDERDOS <5478388+shonderdos@users.noreply.github.com>
2023-12-19 12:06:52 +0100

    feat(dark-mode): replace rxjs subjects for angular signals

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { DarkModePreference, DarkModeService } from '../shared/services/dark-mode/dark-mode.service';
+import { DarkModeService } from '../shared/services/dark-mode/dark-mode.service';
 import { AsyncPipe } from '@angular/common';
 import { PageWrapperComponent } from '../shared/ui/page-wrapper/page-wrapper.component';
 import { PanelComponent } from '../shared/ui/panel/panel.component';
@@ -13,10 +13,5 @@ import { PanelComponent } from '../shared/ui/panel/panel.component';
   imports: [AsyncPipe, PageWrapperComponent, PanelComponent],
 })
 export default class SettingsComponent {
-  private darkModeService = inject(DarkModeService);
-  public darkModePreference = this.darkModeService.preference;
-
-  updatePreference(value: DarkModePreference) {
-    this.darkModeService.updatePreference(value);
-  }
+  public readonly darkMode = inject(DarkModeService);
 }
