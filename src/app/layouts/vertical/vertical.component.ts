7e1bf1b6b97a65efae6af0eca86e9ceb70dea24c
SERGE HONDERDOS <5478388+shonderdos@users.noreply.github.com>
2024-01-24 09:26:43 +0100

    feat(navigation): add styling to vertical nav

--- src/app/layouts/vertical/vertical.component.ts
+++ src/app/layouts/vertical/vertical.component.ts
@@ -2,6 +2,8 @@ import { ChangeDetectionStrategy, Component, effect, HostBinding, inject } from 
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from '../../shared/ui/navigation/navigation.component';
 import { NavigationService } from '../../shared/services/navigation/navigation.service';
+import { NavigationItemComponent } from '../../shared/ui/navigation-item/navigation-item.component';
+import { NAVIGATION_TOKEN } from '../../app.component';
 
 @Component({
   selector: 'app-vertical',
@@ -9,11 +11,12 @@ import { NavigationService } from '../../shared/services/navigation/navigation.s
   styleUrls: ['./vertical.component.scss'],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [NavigationComponent, RouterOutlet],
+  imports: [NavigationComponent, RouterOutlet, NavigationItemComponent],
 })
 export default class VerticalComponent {
   @HostBinding('class') class = 'navigation-closed';
   private state = inject(NavigationService).isOpen;
+  public navigationItems = inject(NAVIGATION_TOKEN);
   constructor() {
     effect(() => {
       this.class = this.state() ? 'navigation-opened' : 'navigation-closed';
