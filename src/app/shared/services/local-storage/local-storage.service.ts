b87d9846f958d8fe1e038a4f30da8e2fcf59220d
SERGE HONDERDOS <5478388+shonderdos@users.noreply.github.com>
2024-01-31 09:53:54 +0100

    feat(navigation) save navigation state in local storage

--- src/app/shared/services/local-storage/local-storage.service.ts
+++ src/app/shared/services/local-storage/local-storage.service.ts
@@ -3,10 +3,20 @@ import { Injectable } from '@angular/core';
 @Injectable({ providedIn: 'root' })
 export class LocalStorageService {
   public get<T>(key: string): T | null {
-    return localStorage.getItem(key) as T;
+    const result = localStorage.getItem(key);
+    if (result === null) {
+      return null;
+    }
+    if (['true', 'false'].includes(result)) {
+      return result === 'true' ? (true as T) : (false as T);
+    }
+    return result as T;
   }
 
-  public set(key: string, value: string): void {
+  public set(key: string, value: string | boolean): void {
+    if (typeof value === 'boolean') {
+      value = value.toString();
+    }
     localStorage.setItem(key, value);
   }
 }
