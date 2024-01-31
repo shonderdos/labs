7c3a68f977a6b15ec1521d97c370c1afd6716df3
SERGE HONDERDOS <5478388+shonderdos@users.noreply.github.com>
2024-01-31 09:53:54 +0100

    feat(navigation) save navigation state in local storage

--- src/app/shared/services/navigation/navigation.service.ts
+++ src/app/shared/services/navigation/navigation.service.ts
@@ -1,10 +1,18 @@
-import { Injectable, Signal, signal } from '@angular/core';
+import { effect, inject, Injectable, Signal, signal } from '@angular/core';
+import { LocalStorageService } from '../local-storage/local-storage.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class NavigationService {
+  private readonly localStorageService = inject(LocalStorageService);
+  private readonly localStorageKey = 'isNavigationOpened';
   private readonly isOpened = signal(false);
+
+  constructor() {
+    this.isOpened.set(this.localStorageService.get<boolean>(this.localStorageKey) ?? false);
+    effect(() => this.localStorageService.set(this.localStorageKey, this.isOpened()));
+  }
 
   public get isOpen(): Signal<boolean> {
     return this.isOpened.asReadonly();
